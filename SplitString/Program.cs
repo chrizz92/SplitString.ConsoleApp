e97375ed0c38697f8219ec6783061f790c08a3ba
chrizz92 <christianschadler@gmx.at>
2020-01-12 22:10:55 +0100

    3 Arten von Split

--- SplitString/Program.cs
+++ SplitString/Program.cs
@@ -7,8 +7,46 @@ namespace SplitString
         static void Main(string[] args)
         {
             string inputText,inputChar;
+            string[] splittedString;
             char splitChar;
-            int splitCounter=0;
+            int splitCounter=0,splittedStringIndex=1;
+
+            //METHODE FÜR STRING-SPLIT
+            string[] StringSplitter(string text,char delimiter)
+            {
+                string[] value;
+                int delCount = 1;
+                int valueIndex = 0;
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == delimiter)
+                    {
+                        delCount++;
+                    }
+                }
+
+                value = new string[delCount];
+
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (text[j] == delimiter)
+                    {
+                        valueIndex++;
+                    }
+                    else
+                    {
+                        value[valueIndex] += text[j];
+                    }
+                }
+
+                return value;
+            }
+
+
+
+
+
 
             Console.WriteLine("Strings trennen");
             Console.WriteLine("===============");
@@ -23,20 +61,45 @@ namespace SplitString
 
             splitChar = Convert.ToChar(inputChar);
 
+            //SELBST VERFASST
             for (int i = 0; i < inputText.Length; i++)
             {
                 if (inputText[i] == splitChar)
                 {
-                    splitCounter++;
+                    splittedStringIndex++;
                 }
             }
 
-            string[] splittedString = inputText.Split(splitChar, splitCounter);
+            splittedString = new string[splittedStringIndex];
+            
+            for (int k = 0; k < inputText.Length; k++)
+            {
+                if (inputText[k] == splitChar)
+                {
+                    Console.WriteLine(splittedString[splitCounter]);
+                    splitCounter++;
+                }
+                else
+                {
+                    splittedString[splitCounter] += inputText[k];
+                }
+            }
+            Console.WriteLine(splittedString[splitCounter]);
 
+            //AUSGABE MIT DER VORGEFERTIGTEN SPLIT-METHODE
+            splittedString = inputText.Split(splitChar);
             for (int j = 0; j < splittedString.Length; j++)
             {
                 Console.WriteLine(splittedString[j]);
             }
+
+            //AUSGABE MIT EIGENER METHODE
+            splittedString = StringSplitter(inputText, splitChar);
+            for (int m = 0; m < splittedString.Length; m++)
+            {
+                Console.WriteLine(splittedString[m]);
+            }
+
         }
     }
 }

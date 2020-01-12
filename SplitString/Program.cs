c2e30d1cfd6cbe5e0a7faba34e24ca9635fb5b75
chrizz92 <christianschadler@gmx.at>
2020-01-12 21:33:08 +0100

    Add project files.

---
+++ SplitString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SplitString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText,inputChar;
+            char splitChar;
+            int splitCounter=0;
+
+            Console.WriteLine("Strings trennen");
+            Console.WriteLine("===============");
+            Console.Write("Bitte Text eingeben: ");
+            inputText = Console.ReadLine();
+            do
+            {
+                Console.Write("Bitte Trennzeichen eingeben: ");
+                inputChar = Console.ReadLine();
+            } 
+            while (inputChar.Length > 1);
+
+            splitChar = Convert.ToChar(inputChar);
+
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                if (inputText[i] == splitChar)
+                {
+                    splitCounter++;
+                }
+            }
+
+            string[] splittedString = inputText.Split(splitChar, splitCounter);
+
+            for (int j = 0; j < splittedString.Length; j++)
+            {
+                Console.WriteLine(splittedString[j]);
+            }
+        }
+    }
+}
